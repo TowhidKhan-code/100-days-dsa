031a4ac9079f2ff34812d4cf9b80e76671f551f9
TowhidKhan-code <towhidkhan586@gmail.com>
2026-02-22 19:59:04 +0530

    replcaed switch with enhanced switch

--- src/javabasics/Day3.java
+++ src/javabasics/Day3.java
@@ -146,26 +146,17 @@ public class Day3 {
        String text = sc.nextLine();
        char operator = text.charAt(0);
        double result = 0;
-       switch(operator){
-           case '+':
-               result = num1 + num2;
-           break;
-           case '-':
-               result = num1 - num2;
-           break;
-           case '*':
-               result = num1 * num2;
-           break;
-           case '/':
-               if(num2 != 0) {
+       switch (operator) {
+           case '+' -> result = num1 + num2;
+           case '-' -> result = num1 - num2;
+           case '*' -> result = num1 * num2;
+           case '/' -> {
+               if (num2 != 0) {
                    result = num1 / num2;
                }
-           break;
-           case '%':
-               result = num1 % num2;
-           break;
-           default:
-               System.out.println("Enter a valid operator!");
+           }
+           case '%' -> result = num1 % num2;
+           default -> System.out.println("Enter a valid operator!");
        }
        System.out.println(result);
 
